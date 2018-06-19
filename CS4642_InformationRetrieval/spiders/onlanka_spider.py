36295ba668a6c7c06a667d7d83ce134b8d552cdd
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-06-19 15:55:25 +0530

    feat: add newly scraped data

--- CS4642_InformationRetrieval/spiders/onlanka_spider.py
+++ CS4642_InformationRetrieval/spiders/onlanka_spider.py
@@ -2,7 +2,7 @@ import scrapy
 
 
 class OnlankaSpider(scrapy.Spider):
-    name = "Onlanka"
+    name = "onlanka"
 
     start_urls = [
         'https://www.onlanka.com/'
@@ -11,16 +11,13 @@ class OnlankaSpider(scrapy.Spider):
     def parse(self, response):
         for news_item in response.css('div.post.type-post'):
             yield {
-                # posts[0].css('a::attr(title)').extract_first()
                 'title': news_item.css('a::attr(title)').extract_first(),
                 'content': news_item.css('div.post-content p::text').extract(),
-                'link': news_item.css('div.read-more a::attr(href)').extract_first()
-
+                'link': news_item.css('div.read-more a::attr(href)').extract_first(),
+                'date_published': news_item.css('span.published::text').extract_first(),
+                'category': news_item.css('div.post-category a::text').extract_first()
             }
 
         next_page = response.css('div.previous a::attr(href)').extract_first()
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
-
-
-
