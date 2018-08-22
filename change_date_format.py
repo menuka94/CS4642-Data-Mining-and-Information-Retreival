e6b0eccea3d41541893ab640478ada48ca1bba9f
Menuka Warushavithana <menuka.14@cse.mrt.ac.lk>
2018-08-23 00:47:17 +0530

    feat(solr): add schema.xml

---
+++ change_date_format.py
@@ -0,0 +1,18 @@
+import json
+import os
+from datetime import datetime
+
+with open('data.json') as f:
+    data = json.load(f)
+
+i = 1
+for record in data:
+    current_date = record['date_published']
+    record['date_published'] = datetime.strptime(current_date, "%B %d, %Y").strftime("%d%m%Y")
+    i += 1
+
+
+f = open("news1.json", 'w+')
+json.dump(data, f)
+f.flush()
+f.close()
